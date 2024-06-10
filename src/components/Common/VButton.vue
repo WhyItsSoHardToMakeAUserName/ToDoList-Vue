a2b925eed270fd9c374bdf77653f909adf64aae9
WhyItsSoHardToMakeAUserName <fox125j83773@gmail.com>
2024-06-10 15:27:10 +0500

    Custom Button Added

--- src/components/Common/VButton.vue
+++ src/components/Common/VButton.vue
@@ -1,13 +1,29 @@
 <template>
-    <div>
-        
-    </div>
+    <button :style>
+        <slot></slot>
+    </button>
 </template>
 <script>
 export default {
-    
+    props:{
+        style:Object,
+    }
 }
 </script>
 <style scoped>
-    
-</style>
+    button{
+        border: none;
+        border-radius: 25px;
+
+        padding: 7px 20px;
+        
+        color: white;
+
+        background-color: #647bfa;
+        transition: all 0.3s
+    }
+    button:hover{
+        background-color: #435ffd;
+        transition: all 0.3s
+    }
+</style> 
